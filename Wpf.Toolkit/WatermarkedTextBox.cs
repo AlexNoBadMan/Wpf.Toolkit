177e7694cd3bdab31e83427038f5ed55676501bd
wosanek <wosanek@gmail.com>
2021-08-27 09:39:24 +0600

    Добавлен textbox с водяным знаком

---
+++ Wpf.Toolkit/WatermarkedTextBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Wpf.Toolkit
+{
+    public class WatermarkedTextBox : TextBox
+    {
+        public string WatermarkText
+        {
+            get { return (string)GetValue(WatermarkTextProperty); }
+            set { SetValue(WatermarkTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty WatermarkTextProperty =
+            DependencyProperty.Register("WatermarkText", typeof(string), typeof(WatermarkedTextBox), new UIPropertyMetadata(null));
+
+        public Brush WatermarkForeground
+        {
+            get { return (Brush)GetValue(WatermarkForegroundProperty); }
+            set { SetValue(WatermarkForegroundProperty, value); }
+        }
+
+        public static readonly DependencyProperty WatermarkForegroundProperty =
+            DependencyProperty.Register("WatermarkForeground", typeof(Brush), typeof(WatermarkedTextBox), new UIPropertyMetadata(Brushes.Gray, null));
+        
+        static WatermarkedTextBox()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(WatermarkedTextBox), new FrameworkPropertyMetadata(typeof(WatermarkedTextBox)));
+        }
+    }
+}
